aab265f3ff660513ae41ff92212703d4496961de
Engin Karataş <enginkaratas99@gmail.com>
2023-01-21 15:14:00 +0300

    Add ground

--- src/sprite/character/Moly.ts
+++ src/sprite/character/Moly.ts
@@ -2,6 +2,7 @@ import p5, { Vector } from "p5";
 import { ImageCharacter } from "./ImageCharacter";
 import { Environment } from "../../utils/Environment";
 import { Skill } from "../abstract/Skill";
+import { EnvironmentConstants } from "../../utils/physics/EnvironmentConstants";
 
 export class Moly extends ImageCharacter implements Skill {
   constructor(p5: p5, position: Vector, imagePath: string) {
@@ -18,14 +19,18 @@ export class Moly extends ImageCharacter implements Skill {
   }
   move(): void {
     if (this._movingRight) {
-      this._pos.add(10, 0);
+      this._pos.add(this._speed, 0);
     } else if (this._movingLeft) {
-      this._pos.x -= 10;
+      this._pos.add(-this._speed, 0);
     }
     this._pos.add(0, this._jumpAcc);
-    this._jumpAcc += Environment.gravity;
+    this._jumpAcc += EnvironmentConstants.gravity;
 
-    this._pos.y = this._p5.constrain(this._pos.y, 0, this._p5.height);
+    this._pos.y = this._p5.constrain(
+      this._pos.y,
+      0,
+      this._p5.height - this._size / 2 - 9
+    );
     this._pos.x = this._p5.constrain(this._pos.x, 0, this._p5.width);
 
     //provide memory leak
