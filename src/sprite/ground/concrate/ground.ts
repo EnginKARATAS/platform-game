a3aabe2d2fc48af4a5de77143cdc24a8d72622df
Engin Karataş <enginkaratas99@gmail.com>
2023-01-21 14:52:32 +0300

    test feature

---
+++ src/sprite/ground/concrate/ground.ts
@@ -0,0 +1,44 @@
+import p5, { Vector } from "p5";
+import { Skill } from "../../abstract/Skill";
+
+export class Ground {
+  _p5: p5;
+  _size: Vector;
+  _position: Vector;
+  _color?: string;
+  constructor(p5: p5, platformSize: Vector, position: Vector) {
+    this._p5 = p5;
+    this._size = platformSize;
+    this._position = position;
+  }
+  getPos(): Vector {
+    return this._position;
+  }
+
+  getSize(): Vector {
+    return this._size;
+  }
+  intersect(
+    characterPos: Skill,
+    objPos: p5.Vector,
+    objSize: p5.Vector
+  ): boolean {
+    //A, B, C, D is diagonal of the rect object
+    let A = characterPos.getPos().x > objPos.x;
+    let B = characterPos.getPos().x < objPos.x + objSize.x;
+    let C = characterPos.getPos().y > objPos.y;
+    let D = characterPos.getPos().y < objPos.y + objSize.y;
+
+    if (A && B && C && D) return true;
+    else return false;
+  }
+
+  draw() {
+    const p5 = this._p5; // just for convenience
+
+    p5.fill("rgb(0, 255, 0)");
+    p5.translate(this._position);
+    p5.noStroke();
+    p5.rect(0, 0, this._size.x, this._size.y);
+  }
+}
